eb0eabcb37e3347a63623b78c27f1947e9860ff5
Brent Bain <brent@anyscale.com>
2024-10-09 10:04:45 -0700

    upd: google-anyscale-iam - additional permissions for cloud storage
    The new K8s offering from Anyscale requires that the data plane
    service account support additional permissions for cloud storage.
    
    Additional small improvements to outputs to also make it easier for
    service account bindings/members.
    
    On branch brent/upd-cloudstorage-iam
    Changes to be committed:
    	modified:   modules/google-anyscale-iam/README.md
    	modified:   modules/google-anyscale-iam/main.tf
    	modified:   modules/google-anyscale-iam/outputs.tf
    	modified:   modules/google-anyscale-iam/roles.tf

--- modules/google-anyscale-iam/outputs.tf
+++ modules/google-anyscale-iam/outputs.tf
@@ -1,36 +1,41 @@
 # ----------------------------------------------
-# Anyscale Cross Account Access Service Account
+# Anyscale Control Plane Service Account
 # ----------------------------------------------
 output "iam_anyscale_access_service_acct_id" {
-  description = "Anyscale cross account access service account ID"
+  description = "Anyscale control plane service account ID"
   value       = try(google_service_account.anyscale_access_service_acct[0].id, null)
 }
 
 output "iam_anyscale_access_service_acct_email" {
-  description = "Anyscale cross account access service account email"
+  description = "Anyscale control plane service account email"
   value       = try(google_service_account.anyscale_access_service_acct[0].email, null)
 }
 
 output "iam_anyscale_access_service_acct_name" {
-  description = "Anyscale cross account access service account name"
+  description = "Anyscale control plane service account name"
   value       = try(google_service_account.anyscale_access_service_acct[0].name, null)
 }
 
 output "iam_anyscale_access_service_acct_unique_id" {
-  description = "Anyscale cross account access service account unique ID"
+  description = "Anyscale control plane service account unique ID"
   value       = try(google_service_account.anyscale_access_service_acct[0].unique_id, null)
+}
+
+output "iam_anyscale_access_service_acct_member" {
+  description = "Anyscale control plane service account member in the form of `serviceAccount:{email}`"
+  value       = try(google_service_account.anyscale_access_service_acct[0].member, null)
 }
 
 # -------------------------------------------
 # Anyscale Workload Identity Pool
 # -------------------------------------------
 output "iam_workload_identity_pool_id" {
-  description = "Anyscale cross account workload identity pool ID"
+  description = "Anyscale control plane workload identity pool ID"
   value       = try(google_iam_workload_identity_pool.anyscale_pool[0].id, null)
 }
 
 output "iam_workload_identity_pool_name" {
-  description = "Anyscale cross account workload identity pool name"
+  description = "Anyscale control plane workload identity pool name"
   value       = try(google_iam_workload_identity_pool.anyscale_pool[0].name, null)
 }
 
@@ -38,11 +43,11 @@ output "iam_workload_identity_pool_name" {
 # Anyscale Workload Identity Provider
 # -------------------------------------------
 output "iam_workload_identity_provider_id" {
-  description = "Anyscale cross account workload identity provider ID"
+  description = "Anyscale control plane workload identity provider ID"
   value       = try(google_iam_workload_identity_pool_provider.anyscale_pool[0].id, null)
 }
 output "iam_workload_identity_provider_name" {
-  description = "Anyscale cross account workload identity provider name"
+  description = "Anyscale control plane workload identity provider name"
   value       = try(google_iam_workload_identity_pool_provider.anyscale_pool[0].name, null)
 }
 
@@ -50,21 +55,26 @@ output "iam_workload_identity_provider_name" {
 # Anyscale Cluster Node Service Account
 # -------------------------------------------
 output "iam_anyscale_cluster_node_service_acct_id" {
-  description = "Anyscale cross account cluster node service account ID"
+  description = "Anyscale data plane cluster node service account ID"
   value       = try(google_service_account.anyscale_cluster_node_service_acct[0].id, null)
 }
 
 output "iam_anyscale_cluster_node_service_acct_email" {
-  description = "Anyscale cross account cluster node service account email"
+  description = "Anyscale data plane cluster node service account email"
   value       = try(google_service_account.anyscale_cluster_node_service_acct[0].email, null)
 }
 
 output "iam_anyscale_cluster_node_service_acct_name" {
-  description = "Anyscale cross account cluster node service account name"
+  description = "Anyscale data plane cluster node service account name"
   value       = try(google_service_account.anyscale_cluster_node_service_acct[0].name, null)
 }
 
 output "iam_anyscale_cluster_node_service_acct_unique_id" {
-  description = "Anyscale cross account cluster node service account unique ID"
+  description = "Anyscale data plane cluster node service account unique ID"
   value       = try(google_service_account.anyscale_cluster_node_service_acct[0].unique_id, null)
+}
+
+output "iam_anyscale_cluster_node_service_acct_member" {
+  description = "Anyscale data plane cluster node service account member  in the form of `serviceAccount:{email}`"
+  value       = try(google_service_account.anyscale_cluster_node_service_acct[0].member, null)
 }
