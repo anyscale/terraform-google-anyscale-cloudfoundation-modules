821ccdabe4eade18ceb28802ec63cae8d8457639
Brent Bain <brent@anyscale.com>
2024-10-17 18:22:43 -0400

    upd: GKE Sub-Module with additional configuration options On branch brent/gke-standard Changes to be committed: 	modified:   ../README.md 	modified:   README.md 	modified:   main.tf 	modified:   variables.tf 	modified:   ../main.tf 	modified:   ../outputs.tf 	modified:   ../variables.tf

--- modules/google-anyscale-gke/examples/main.tf
+++ modules/google-anyscale-gke/examples/main.tf
@@ -19,7 +19,10 @@ locals {
 module "anyscale_cloudapis" {
   source         = "../../google-anyscale-cloudapis"
   module_enabled = true
+
+  anyscale_activate_optional_apis = ["container.googleapis.com"]
 }
+
 module "anyscale_vpc" {
   #checkov:skip=CKV_GCP_61:VPC Flow Logs are not required for this example.
   source         = "../../google-anyscale-vpc"
@@ -28,12 +31,32 @@ module "anyscale_vpc" {
   private_subnet_cidr     = "172.21.100.0/22"
   gke_services_range_cidr = "172.21.104.0/22"
   gke_pod_range_cidr      = "172.21.108.0/22"
+
+  depends_on = [module.anyscale_cloudapis]
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
 # all_defaults
-#   Create a GKE Cluster with all defaults
+#   Create a GKE Cluster with as many defaults as possible
+#   - Standard GKE Cluster
+#   - No additional node pools
 # ---------------------------------------------------------------------------------------------------------------------
+module "all_defaults_iam" {
+  #checkov:skip=CKV_TF_1: Example code should use the latest version of the module
+  #checkov:skip=CKV_TF_2: Example code should use the latest version of the module
+  source         = "../../google-anyscale-iam"
+  module_enabled = true
+
+  anyscale_org_id                           = var.anyscale_organization_id
+  create_anyscale_access_role               = true
+  create_anyscale_cluster_node_service_acct = true # Set to true to bind to a GKE Service Account
+  anyscale_cluster_node_service_acct_name   = "anyscale-cluster-node"
+
+  anyscale_project_id = var.google_project_id
+
+  depends_on = [module.anyscale_cloudapis]
+}
+
 # trivy:ignore:avd-gcp-0051 # This is an example of a GKE Cluster with all defaults.
 # trivy:ignore:avd-gcp-0056 # This is an example of a GKE Cluster with all defaults.
 # trivy:ignore:avd-gcp-0059 # This is an example of a GKE Cluster with all defaults.
@@ -43,16 +66,18 @@ module "all_defaults" {
   # checkov:skip=CKV_GCP_61:VPC Flow Logs are not required for this example.
   source         = "../"
   module_enabled = true
-  depends_on = [
-    module.anyscale_cloudapis
-  ]
 
   gke_cluster_vpc        = module.anyscale_vpc.vpc_selflink
   gke_cluster_subnet     = module.anyscale_vpc.private_subnet_selflink
   ip_range_name_pods     = module.anyscale_vpc.private_subnet_gke_pod_range_name
   ip_range_name_services = module.anyscale_vpc.private_subnet_gke_services_range_name
 
-  deletion_protection = false
+  anyscale_project_id                 = var.google_project_id
+  gke_cluster_gcp_iam_service_account = module.all_defaults_iam.iam_anyscale_cluster_node_service_acct_email
+
+  deletion_protection = false # Default is true which does not allow TF to destroy the GKE Cluster
 
   labels = local.full_labels
+
+  depends_on = [module.anyscale_cloudapis]
 }
--- modules/google-anyscale-gke/outputs.tf
+++ modules/google-anyscale-gke/outputs.tf
@@ -4,20 +4,36 @@
 
 output "anyscale_gke_cluster_id" {
   description = "Anyscale GKE Cluster ID"
-  value       = try(google_container_cluster.anyscale_dataplane[0].id, null)
+  value = try(
+    google_container_cluster.anyscale_dataplane_standard[0].id,
+    google_container_cluster.anyscale_dataplane_autopilot[0].id,
+    null
+  )
 }
 
 output "anyscale_gke_cluster_name" {
   description = "Anyscale GKE Cluster name"
-  value       = try(google_container_cluster.anyscale_dataplane[0].name, null)
+  value = try(
+    google_container_cluster.anyscale_dataplane_standard[0].name,
+    google_container_cluster.anyscale_dataplane_autopilot[0].name,
+    null
+  )
 }
 
 output "anyscale_gke_cluster_endpoint" {
   description = "Anyscale GKE Cluster endpoint"
-  value       = try(google_container_cluster.anyscale_dataplane[0].endpoint, null)
+  value = try(
+    google_container_cluster.anyscale_dataplane_standard[0].endpoint,
+    google_container_cluster.anyscale_dataplane_autopilot[0].endpoint,
+    null
+  )
 }
 
 output "anyscale_gke_cluster_master_version" {
   description = "Anyscale GKE Cluster master version"
-  value       = try(google_container_cluster.anyscale_dataplane[0].master_version, null)
+  value = try(
+    google_container_cluster.anyscale_dataplane_standard[0].master_version,
+    google_container_cluster.anyscale_dataplane_autopilot[0].master_version,
+    null
+  )
 }
