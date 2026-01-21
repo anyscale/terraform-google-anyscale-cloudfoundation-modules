37689d2938578011e251be8ec3305721f4409765
Sahil Khadilkar <sahil.khadilkar@sahilkhadilkar-GL09PFCYGY.local>
2026-01-21 12:04:41 +0530

    added logic to lookup for vpc_id if firewall is enabled and/or VPC name is given

--- data.tf
+++ data.tf
@@ -13,3 +13,15 @@ data "google_compute_subnetwork" "shared_vpc_subnet" {
 
   name = var.existing_vpc_subnet_name
 }
+
+data "google_compute_network" "existing_vpc" {
+  count   = local.execute_vpc_firewall_sub_module && var.existing_vpc_name != null && var.existing_vpc_id == null && var.existing_project_id != null && var.shared_vpc_project_id == null ? 1 : 0
+  project = var.existing_project_id
+  name    = var.existing_vpc_name
+}
+
+data "google_compute_network" "shared_vpc" {
+  count   = local.execute_vpc_firewall_sub_module && var.existing_vpc_name != null && var.existing_vpc_id == null && var.shared_vpc_project_id != null ? 1 : 0
+  project = var.shared_vpc_project_id
+  name    = var.existing_vpc_name
+}
--- main.tf
+++ main.tf
@@ -183,7 +183,12 @@ module "google_anyscale_vpc_firewall_policy" {
   anyscale_project_id = local.vpc_project_id
 
   vpc_name = local.execute_vpc_firewall_sub_module ? coalesce(var.existing_vpc_name, module.google_anyscale_vpc.vpc_name) : ""
-  vpc_id   = local.execute_vpc_firewall_sub_module ? coalesce(var.existing_vpc_id, module.google_anyscale_vpc.vpc_id) : ""
+  vpc_id   = local.execute_vpc_firewall_sub_module ? coalesce(
+    var.existing_vpc_id,
+    try(data.google_compute_network.existing_vpc[0].self_link, null),
+    try(data.google_compute_network.shared_vpc[0].self_link, null),
+    module.google_anyscale_vpc.vpc_id
+  ) : ""
 
   firewall_policy_name        = local.firewall_policy_name
   firewall_policy_description = var.anyscale_vpc_firewall_policy_description
